8cf5f6cc24fba48609bcc14d8d97dd14177ace4f
hkichen <hkichen@gmail.com>
2018-05-08 13:44:15 -0400

    server js set up done

--- server.js
+++ server.js
@@ -4,10 +4,24 @@ var path = require("path");
 
 //==Express App==//
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({
      extended: true
 }));
 
-app.use(bodyParsery.json());
+//set express to handle parsing
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/app/public"));
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+//Require routing to api and html
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+//Start the server to begin listening
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
