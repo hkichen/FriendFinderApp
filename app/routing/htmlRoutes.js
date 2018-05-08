20fb313eb07a8afc345c1e3bbe678b3085ada868
hkichen <hkichen@gmail.com>
2018-05-08 15:33:50 -0400

    added paths to html pages

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,15 @@
+var path = require('path');
+
+module.exports = function (app) {
+
+	app.get('/survey', function(req, res) {
+		res.sendFile(path.join(__dirname + '/../public/survey.html'))
+	});
+	// sends user to homepage
+	app.use('/', function(req, res) {
+		res.sendFile(path.join(__dirname + '/../public/home.html'))
+	});
+
+}
+
+//when clicking on button, direct user to survey or home page
