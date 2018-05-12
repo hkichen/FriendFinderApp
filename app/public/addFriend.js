b10a17e11db6ae556edd56492541bfd244a08dfc
hkichen <hkichen@gmail.com>
2018-05-12 19:32:22 -0400

    fixed submit butn, getting 403 error now

--- app/public/addFriend.js
+++ app/public/addFriend.js
@@ -5,7 +5,7 @@ $("#submitBtn").on("click", function() {
   event.preventDefault();
   var newFriend = {
     "name": $("#name").val().trim(),
-    "photo": $("#photo").val().trim(),
+    "photo": $("#photo").val(),
     "scores": [$("#q1").val(), $("#q2").val(), $("#q3").val(), $("#q4").val(), $("#q5").val(), $("#q6").val(), $("#q7").val(), $("#q8").val(), $("#q9").val(), $("#q10").val() ]
   };
   alert("Searching for your match...");
